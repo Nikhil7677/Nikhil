968dcbe3300f11ada3b9cd8f278113c00316bda9
Nikhil7677 <mothukurinikhil2277@msitprogram.net>
2024-07-24 10:04:08 +0530

    Add feature to save data to CSV

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@
 # scraper.py
 
 import requests
+import csv
 
 from bs4 import BeautifulSoup
 
@@ -16,4 +17,11 @@ soup = BeautifulSoup(response.text, 'html.parser')
  
 
 print(soup.title.text)
+with open('data.csv', 'w', newline='') as file:
+
+    writer = csv.writer(file)
+
+    writer.writerow(["Title"])
+
+    writer.writerow([soup.title.text])
 
